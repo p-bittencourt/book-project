d33ba6f4d732cb1ee04adf08f275ede10f070045
p-bittencourt <pedromonteirobittencourt@gmail.com>
2025-10-11 23:00:46 +0000

    appropriately accesses ID, rating and comment from req.params and req.data

--- srv/service.js
+++ srv/service.js
@@ -1,4 +1,5 @@
-const cds = require('@sap/cds')
+const cds = require('@sap/cds');
+const { SELECT } = require('@sap/cds/lib/ql/cds-ql');
 const logger = cds.log('BookRaterService')
 
 class BookRaterService extends cds.ApplicationService {
@@ -9,16 +10,34 @@ class BookRaterService extends cds.ApplicationService {
         this.on('rateBook', Books, (req) => rateBook(req))
 
         /**
-         * We should validate that the book exists
-         * then, register the rating.
-         * 
+         * We should validate that the book exists then register the rating.
+         *  
          * Book averageRating should reflect the change.
          * Should this be done on an after handler maybe?
          * 
          */
         async function rateBook(req) {
-            logger('Params' + JSON.stringify(req.params))
-            logger('Data' + JSON.stringify(req.data))
+            const { ID }  = req.params[0];
+            const { rating, comment } = req.data;
+
+            /**
+             * What's the try catch block to handle this:
+             * 
+             * {
+                    "error": {
+                        "message": "near \"\"$B\"\": syntax error in:\nSELECT json_insert('{}','$.\"ID\"',ID,'$.\"title\"',title,'$.\"descr\"',descr,'$.\"averageRating\"',averageRating) as _json_ FROM (SELECT \"$B\".ID,\"$B\".title,\"$B\".descr,\"$B\".averageRating FROM BookRater_Books as \"$B\" WHERE \"$B\".ID \"$B\".ID = ?)",
+                        "code": "SQLITE_ERROR",
+                        "@Common.numericSeverity": 4
+                    }
+                }
+             */
+            const book = await SELECT.one().from(Books).where({ID: ID})
+
+            if (!book) {
+                logger('Book not found')
+            }
+
+            logger(rating, comment)
         }
 
         return super.init()
