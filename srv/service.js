a513d7955c3318c29e3dd4c8289dadcc59d7ef67
p-bittencourt <pedromonteirobittencourt@gmail.com>
2025-10-20 17:43:52 +0000

    feat: recalculates averageRating when posting a new rating

--- srv/service.js
+++ srv/service.js
@@ -6,16 +6,33 @@ class BookRaterService extends cds.ApplicationService {
 
     async init() {
         this.on(['CREATE', 'UPDATE'], 'Books', (req, next) => {
-            logger(req);
+            const ratingsInput = req.query['INSERT'].entries[0].Ratings
+            let averageRating = null
+            if (ratingsInput && ratingsInput.length > 0) {
+                const ratings = ratingsInput.map(ratingValue => ratingValue.rating)
+                if (ratings) {
+                    averageRating = this.calculateAverageRating(ratings);
+                }
+            }
+            req.query['INSERT'].entries[0].averageRating = averageRating;
             return next();
         })
 
-        this.on(['CREATE', 'UPDATE'], 'Books.Ratings', (req, next) => {
-            logger(req);
-            return next();
+        this.after(['CREATE', 'UPDATE'], 'Books.Ratings', async (results, req) => {
+            const book = await SELECT.from('Books', req.params[0], b => {
+                b('Ratings'),
+                b.Ratings(r => r('rating'))
+            })
+            const ratingValues = book.Ratings.map(ratingValue => ratingValue.rating)
+            const averageRating = this.calculateAverageRating(ratingValues)
+            logger(averageRating)
         })
 
         return super.init()
+    }
+
+    calculateAverageRating(ratings) {
+        return ratings.length > 0 ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length : undefined;
     }
 
 }
